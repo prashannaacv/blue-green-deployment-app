de355a0b5d7c8fcd3590bbddd4a096563e328909
C V PRASHANNAA <cvprashannaa@gmail.com>
2024-11-08 11:28:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello from Blue-Green Deployment!'));
+app.get('/', (req, res) => res.send('Hello from Blue-Green Deployment'));
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
